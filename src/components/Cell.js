3848a674416da540ef683f04e45cbc737e6fe907
gingus55 <gingus55@hotmail.com>
2023-02-17 12:15:06 +0000

    tetris styles added

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => {
-  return <div>Cell</div>;
+  return (
+    <StyledCell type={type} color={TETROMINOS[type].color}>
+      Cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -61,6 +61,6 @@ export const TETROMINOS = {
 export const randomTetromino = () => {
   const tetrominos = "IJLOSTZ";
   const randTetromino =
-    tetrominos[Math.floor(Math.random() * terominos.length)];
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
   return TETROMINOS[randTetromino];
 };
