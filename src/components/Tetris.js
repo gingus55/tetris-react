ba78d7fdfd3cbcc2713afb0dc65aa59a9e5ea7f6
gingus55 <gingus55@hotmail.com>
2023-02-23 13:35:33 +0000

    collision logic complete

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { createStage } from "../gameHelpers";
+import { createStage, checkCollision } from "../gameHelpers";
 // Styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
@@ -15,25 +15,38 @@ import StartButton from "./StartButton";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
-  const [gameOver, setGameover] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
 
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
 
   const startGame = () => {
     // reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if (player.pos.y < 1) {
+        console.log("GAME OVER");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
 
   const dropPlayer = () => {
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -8,7 +8,7 @@ export const createStage = () =>
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y++) {
-    for (let x = 0; x < player.tertromino[y].length; x++) {
+    for (let x = 0; x < player.tetromino[y].length; x++) {
       // 1. Check we are on a tetromino cell
       if (player.tetromino[y][x] !== 0) {
         if (
